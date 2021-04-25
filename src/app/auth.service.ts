c49bd72c4afa32220d2598c1610e886ade3e0113
Luca Moroni <terraformer.144@gmail.com>
2021-04-25 14:47:18 +0200

    add oauth funcs to auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,36 +1,77 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Observable, of } from 'rxjs';
-import { tap, delay } from 'rxjs/operators';
+import { OAuthService } from 'angular-oauth2-oidc'
+
+import { authPasswordFlowConfig } from './auth-password-flow.config'
+import { googleAuthConfig } from './auth-google.config'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  isLoggedIn: boolean;
   redirectUrl: string;
+  userProfile: object | undefined;
+
+  userName: string;
+  password: string;
 
   constructor (
-    public router: Router
+    private router: Router,
+    private oauthService: OAuthService
   ) {
-    this.isLoggedIn = false;
     this.redirectUrl = "";
+
+    this.userName = "max";
+    this.password = "geheim";
   }
 
-  login(): Observable<boolean> { // la logica di autenticazione va qua
-    return of(true).pipe(
-      delay(1000),
-      tap(val => {
-        this.isLoggedIn = true;
+  oauthGoogleConfig() {
+    this.oauthService.configure(googleAuthConfig);
+    this.oauthService.redirectUri = window.location.origin + '/',
+    this.oauthService.loadDiscoveryDocumentAndTryLogin()
+  }
+
+  oauthPasswordFlowConfig() {
+    this.oauthService.configure(authPasswordFlowConfig);
+    this.oauthService.loadDiscoveryDocument();
+  }
+
+  loadUserProfile(): void {
+    this.oauthService.loadUserProfile().then(up => (this.userProfile = up));
+  }
+
+  get access_token() {
+    return this.oauthService.getAccessToken();
+  }
+
+  get access_token_expiration() {
+    return this.oauthService.getAccessTokenExpiration();
+  }
+
+  loginPersonal() { // la logica di autenticazione va qua
+    this.oauthService
+      .fetchTokenUsingPasswordFlowAndLoadUserProfile(
+        this.userName,
+        this.password
+      )
+      .then(() => {
+        console.debug('successfully logged in');
         this.router.navigate([this.redirectUrl]);
       })
-    );
+      .catch(err => {
+        console.error('error logging in', err);
+      });
+  }
+
+  login() {
+    this.oauthGoogleConfig();
+    this.oauthService.initImplicitFlow();
   }
 
   logout(): void {
-    this.isLoggedIn = false;
+    this.oauthService.logOut(true);
     this.router.navigate(['/login']);
   }
 
