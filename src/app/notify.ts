e9f0aaaeec3629c3b458570feff30dfe9a4bc92e
Luca Moroni <terraformer.144@gmail.com>
2021-06-11 11:55:51 +0200

    update style features

--- src/app/notify.ts
+++ src/app/notify.ts
@@ -2,6 +2,6 @@ export interface Notify {
     id: number;
     user_id: number;
     message: string;
-    action_url: string;
+    action_url: any; //{component: "market" | "band" | "serate", id_obj: number} 
     picture_url: string;
 }
